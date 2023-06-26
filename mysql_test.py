97ed0c3c0d83754edeecc0149a530ab555f1bb96
whudgin <whudgins@forgeddev.com>
2023-06-25 22:59:53 -0500

    initial commit

---
+++ mysql_test.py
@@ -0,0 +1,27 @@
+import mysql.connector
+from mysql.connector import errorcode
+
+config = {
+    "user": "root",
+    "password": "RangerUp1515!",
+    "host": "localhost",
+    "database": "movies",
+    "raise_on_warnings": True
+}
+
+try:
+    db = mysql.connector.connect(**config)
+
+    print("\n Database user {} connected to MySQL on host {} with database {}".format(config["user"], config["host"], config["database"]))
+
+    input("\n\n Press any key to continue...")
+
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("\n Something is wrong with your user name or password")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("\n Database does not exist")
+    else:
+        print(err)
+finally:
+    db.close()
